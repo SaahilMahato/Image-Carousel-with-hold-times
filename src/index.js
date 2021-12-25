6a280bb9671f787a30166eb006240e4830d05377
SaahilMahato <saahilmahato72@gmail.com>
2021-12-25 16:15:22 +0545

    updated web page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 const carousel1 = new Carousel('.carousel-container-1', {transitionTime: 0.5, delayTime: 2});
-const carousel2 = new Carousel('.carousel-container-2', {transitionTime: 3, delayTime: 5});
+const carousel2 = new Carousel('.carousel-container-2', {transitionTime: 2, delayTime: 5});
